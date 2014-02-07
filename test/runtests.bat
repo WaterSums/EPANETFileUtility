d6e3259126c9318e9a44f0ac6c24608c408db97b
WaterSums <marktmorgan@gmail.com>
2014-02-07 22:59:31 +1100

    Some more work on the testing process

--- test/runtests.bat
+++ test/runtests.bat
@@ -1,2 +1,7 @@
+@REM Run basic tests of EPANETOutputFile tool using EPANET Net1 output file
+@setlocal
+set LANG=en_AU
+if not exist output: mkdir output
+del /y output\*.csv
 python ..\ReadEPANETOutputFile.py -vpedca -n output\Net1_pnode.csv -l output\Net1_plink.csv -E output\Net1_e.csv -N output\Net1_dnode.csv -L output\Net1_dlink.csv data\Net1.hyd
-
+@endlocal
