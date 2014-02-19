6742fd8f9d6d1c2a4081579c36a0e895d9a996ea
Mark Morgan <marktmorgan@MTMMBP17.local>
2014-02-19 14:42:30 +1100

    Update i18n and testing on MacOS X
    Update scripts and testing output

--- i18n.sh
+++ i18n.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xgettext -n -d EPANETOutputFile -p EPANETOutputFile -o EPANETOutputFile.pot -L Python EPANETOutputFile/EPANETOutputFile.py EPANETOutputFile/EPANETOutputFilePlugin.py EPANETOutputFile/EOFTInternalPlugin.py
+#xgettext -n -d EPANETOutputFile -p EPANETOutputFile -o EPANETOutputFile.pot -L Python EPANETOutputFile/EPANETOutputFile.py EPANETOutputFile/EPANETOutputFilePlugin.py EPANETOutputFile/EOFTInternalPlugin.py EPANETOutputFile/plugins/demo/__init__.py
+xgettext --copyright-holder="Mark Morgan" --package-name="EPANETOutputFile" --package-version="1.0.0" -n -d EPANETOutputFile -p EPANETOutputFile -o EPANETOutputFile.pot -L Python EPANETOutputFile/EPANETOutputFile.py EPANETOutputFile/EPANETOutputFilePlugin.py EPANETOutputFile/EOFTInternalPlugin.py EPANETOutputFile/plugins/demo/__init__.py
--- test/runtests.sh
+++ test/runtests.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
-# Run basic tests of EPANETOutputFile tool using EPANET Net1 output file
-export LANG=en_AU
+# Run basic tests of EPANETOutputFile tool using EPANET Net[123],hyd output file
 mkdir -p output
-rm output/*.csv
-python ../ReadEPANETOutputFile.py -vpedca -n output/Net1_pnode.csv -l output/Net1_plink.csv -E output/Net1_e.csv -N output/Net1_dnode.csv -L output/Net1_dlink.csv data/Net1.hyd
+rm output/*.csv output/Net*.txt
+LANG=en_AU python ../ReadEPANETOutputFile.py --demo_all -vpedca -n output/Net1_pnode.csv -l output/Net1_plink.csv -E output/Net1_e.csv -N output/Net1_dnode.csv -L output/Net1_dlink.csv data/Net1.hyd > output/Net1.txt 2>&1
+diff output/Net1_e.csv known_output/
+diff output/Net1_pnode.csv known_output/
+diff output/Net1_plink.csv known_output/
+diff output/Net1_dnode.csv known_output/
+diff output/Net1_dlink.csv known_output/
+diff output/Net1_e.csv known_output/
+LANG=en_AU python ../ReadEPANETOutputFile.py --demo_all -vpedca -n output/Net2_pnode.csv -l output/Net2_plink.csv -E output/Net2_e.csv -N output/Net2_dnode.csv -L output/Net2_dlink.csv data/Net2.hyd > output/Net2.txt 2>&1
+diff output/Net2_e.csv known_output/
+diff output/Net2_pnode.csv known_output/
+diff output/Net2_plink.csv known_output/
+diff output/Net2_dnode.csv known_output/
+diff output/Net2_dlink.csv known_output/
+diff output/Net2_e.csv known_output/
+LANG=en_AU python ../ReadEPANETOutputFile.py --demo_all -vpedca -n output/Net3_pnode.csv -l output/Net3_plink.csv -E output/Net3_e.csv -N output/Net3_dnode.csv -L output/Net3_dlink.csv data/Net3.hyd > output/Net3.txt 2>&1
+diff output/Net3_e.csv known_output/
+diff output/Net3_pnode.csv known_output/
+diff output/Net3_plink.csv known_output/
+diff output/Net3_dnode.csv known_output/
+diff output/Net3_dlink.csv known_output/
+diff output/Net3_e.csv known_output/
